74fc11491ca2b6fd59878aa27d224e3f2e19f975
Ed Kellett <edk141@gmail.com>
2014-08-15 15:17:24 +0100

    version 0.1a1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='spy-cli',
-    version='0.1',
+    version='0.1a1',
     description='stream processing Python CLI',
     packages=['spy'],
     zip_safe=True,
