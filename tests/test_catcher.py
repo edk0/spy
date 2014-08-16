697c2c6d47f0af7fb90d7ce1e85e275ea401df2f
Ed Kellett <edk141@gmail.com>
2014-08-16 11:48:01 +0100

    update test_catcher

--- tests/test_catcher.py
+++ tests/test_catcher.py
@@ -11,8 +11,11 @@ def _exception_undecorated(v):
 
 def test_undecorated(capsys):
     seq = [_exception_undecorated]
-    with catcher.handler(exit=False):
-        spy.chain(seq).run_to_exhaustion()
+    try:
+        with catcher.handler(exit=False):
+            spy.chain(seq).run_to_exhaustion()
+    except catcher.CaughtException as e:
+        e.print_traceback()
     out, err = capsys.readouterr()
     assert any(s.startswith('  Fragment 1') for s in err.splitlines())
 
@@ -25,8 +28,11 @@ def _exception_in_decorator(v):
 
 def test_in_decorator(capsys):
     seq = [_exception_in_decorator]
-    with catcher.handler(exit=False):
-        spy.chain(seq).run_to_exhaustion()
+    try:
+        with catcher.handler(exit=False):
+            spy.chain(seq).run_to_exhaustion()
+    except catcher.CaughtException as e:
+        e.print_traceback()
     out, err = capsys.readouterr()
     assert any(s.startswith('  Fragment 1, in decorator spy.decorators.callable') for s in err.splitlines())
 
@@ -40,3 +46,8 @@ def test_hook(capsys):
         hook(*sys.exc_info())
     out, err = capsys.readouterr()
     assert any(s.startswith('  Fragment 1') for s in err.splitlines())
+
+
+def test_caughtexception():
+    c = catcher.CaughtException([])
+    assert str(c) == ''
