edk0/spy

commit b80ae5da5a2169c198e3edcff4869f9f43e5b2e1

more optimization

spy/core.py
@@ -5,8 +5,10 @@ from functools import wraps
 
 from . import fragments
 
+import dis, sys
 
-_iteration_state = []
+
+_iteration_state = None
 
 
 def _call_fragment_body(f, *a, **kw):
@@ -19,19 +21,17 @@ DROP = _Drop()
 
 
 def fragment(fn):
-    @wraps(fn)
+    global _iteration_state
     def fragment(ita, index=None):
         ita = iter(ita)
         _spy_fragment_index = index
-        for item in ita:
-            _spy_value = item
-            _iteration_state.append((item, ita))
-            result = fn(item)
-            _iteration_state.pop()
+        for _spy_value in ita:
+            _iteration_state = (_spy_value, ita)
+            result = fn(_spy_value)
             if result is DROP:
                 continue
             elif isinstance(result, many):
-                for result in iter(result):
+                for result in result.ita:
                     yield result
             else:
                 yield result
@@ -83,5 +83,5 @@ class many:
 
 
 def collect():
-    init, ita = _iteration_state[-1]
+    init, ita = _iteration_state
     return itertools.chain([init], ita)

spy/fragments.py
@@ -3,6 +3,7 @@ from collections import Iterable, Mapping, Sequence
 from itertools import chain, islice
 
 import sys
+py_write = sys.stdout.write
 
 from .objects import SpyFile
 
@@ -28,7 +29,8 @@ def make_limit(*, start=0, end=None):
 
 def pretty_print(thing):
     if isinstance(thing, str):
-        py_print(thing)
+        py_write(thing)
+        py_write('\n')
     elif not isinstance(thing, (Mapping, Sequence)) and isinstance(thing, Iterable):
         sliced = []
         for n, item in enumerate(thing):

spy/cli.py
@@ -28,11 +28,17 @@ def compile_(code, filename='<input>'):
 
 
 def make_callable(code, is_expr, context, debuginfo=(None, None)):
-    def fragment_fn(value):
-        local = context.pipe_view(value)
-        local._spy_debuginfo = debuginfo + (value,)
-        result = eval(code, context, local)
-        return result if is_expr else local.value
+    if is_expr:
+        def fragment_fn(value):
+            local = context.pipe_view(value)
+            local._spy_debuginfo = debuginfo
+            return eval(code, context, local)
+    else:
+        def fragment_fn(value):
+            local = context.pipe_view(value)
+            local._spy_debuginfo = debuginfo
+            eval(code, context, local)
+            return local.value
     fragment_fn._spy_debuginfo = debuginfo
     return fragment_fn
 
@@ -104,6 +110,8 @@ def _main(*steps,
 
     end: Stop after getting this result (zero-based)
     """
+    sys.setcheckinterval(10000)
+
     compiled_steps = []
     imports = set()
     for i, code in enumerate(steps):

commit f20894c3ce46096dde6afb0126ad27f7bdd02dc4

fix bugs introduced in b80ae5d

spy/core.py
@@ -21,8 +21,8 @@ DROP = _Drop()
 
 
 def fragment(fn):
-    global _iteration_state
     def fragment(ita, index=None):
+        global _iteration_state
         ita = iter(ita)
         _spy_fragment_index = index
         for _spy_value in ita:

spy/fragments.py
@@ -3,7 +3,6 @@ from collections import Iterable, Mapping, Sequence
 from itertools import chain, islice
 
 import sys
-py_write = sys.stdout.write
 
 from .objects import SpyFile
 
@@ -29,8 +28,8 @@ def make_limit(*, start=0, end=None):
 
 def pretty_print(thing):
     if isinstance(thing, str):
-        py_write(thing)
-        py_write('\n')
+        _write(thing)
+        _write('\n')
     elif not isinstance(thing, (Mapping, Sequence)) and isinstance(thing, Iterable):
         sliced = []
         for n, item in enumerate(thing):
@@ -45,6 +44,8 @@ def pretty_print(thing):
 
 
 def print(ita):
+    global _write
+    _write = sys.stdout.write
     for item in ita:
         pretty_print(item)
         yield item