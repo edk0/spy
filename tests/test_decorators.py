58700a1c105f9061dbd3f537a1b1401ab33abd12
Ed Kellett <edk141@gmail.com>
2014-08-18 22:57:36 +0100

    test_decorators: add test_many and test_once

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -24,3 +24,28 @@ def test_filter():
 
     assert test('FOO') == spy.DROP
     assert test('BAR') == 'BAR'
+
+
+def test_many():
+    @spy.fragment
+    @decorators.many
+    def test(v):
+        return [1, 2, 3, 4, 5]
+
+    l = spy.chain([test])
+    assert list(l) == [1, 2, 3, 4, 5]
+
+
+def test_once():
+    calls = 0
+
+    @spy.fragment
+    @decorators.once
+    def test(v):
+        nonlocal calls
+        calls += 1
+        return 'a'
+
+    spy.chain([test], ['x'] * 20).run_to_exhaustion()
+
+    assert calls == 1
