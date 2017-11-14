cce17afdf5ca2ee2ac29556df71e667b32727de3
Ed Kellett <e@kellett.im>
2017-11-14 04:15:15 +0000

    tidy up: use pytest's monkeypatching

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -36,7 +36,7 @@ def test_context_builtins():
     eval('[int(x) for x in (1, 2)]', context, context)
 
 
-def test_argument_errors():
+def test_argument_errors(monkeypatch):
     test_inputs = [
         ['-a'],
         ['-a', '-a'],
@@ -46,93 +46,72 @@ def test_argument_errors():
         ['--accumulate', '-a'],
         ['-l', '-a']
     ]
-    stdin = sys.stdin
-    try:
-        sys.stdin = io.StringIO("")
-        for input_ in test_inputs:
-            try:
-                spy.cli._main(sys.argv[0], *input_)
-            except ValueError as e:
-                assert 'No value found after --accumulate' in str(e)
-    finally:
-        sys.stdin = stdin
-
-
-def test_unknown_option():
-    stdin = sys.stdin
-    try:
-        sys.stdin = io.StringIO("")
+    monkeypatch.setattr(sys, 'stdin', io.StringIO(""))
+    for input_ in test_inputs:
         try:
-            spy.cli._main(sys.argv[0], '-a!')
+            spy.cli._main(sys.argv[0], *input_)
         except ValueError as e:
-            assert 'Unknown option' in str(e)
-    finally:
-        sys.stdin = stdin
+            assert 'No value found after --accumulate' in str(e)
 
 
-def test_excepthook(capsys):
-    stdin = sys.stdin
+def test_unknown_option(monkeypatch):
+    monkeypatch.setattr(sys, 'stdin', io.StringIO(""))
     try:
-        sys.stdin = io.StringIO("")
-        try:
-            spy.cli._main(sys.argv[0], '-f', 'this_name_does_not_exist_either')
-            pytest.fail("didn't raise an exception")
-        except Exception:
-            sys.excepthook(*sys.exc_info())
-        out, err = capsys.readouterr()
-        for line in err.splitlines():
-            assert "spy/main.py" not in line
-        assert "  Fragment 1" in err.splitlines()
-        assert "    --filter 'this_name_does_not_exist_either'" in err.splitlines()
-        with pytest.raises(NameError):
-            spy.cli._main(sys.argv[0], '--no-exception-handling', 'still_broken_i_hope')
-    finally:
-        sys.stdin = stdin
+        spy.cli._main(sys.argv[0], '-a!')
+    except ValueError as e:
+        assert 'Unknown option' in str(e)
 
 
-def test_run(capsys):
+def test_excepthook(capsys, monkeypatch):
+    monkeypatch.setattr(sys, 'stdin', io.StringIO(""))
+    try:
+        spy.cli._main(sys.argv[0], '-f', 'this_name_does_not_exist_either')
+        pytest.fail("didn't raise an exception")
+    except Exception:
+        sys.excepthook(*sys.exc_info())
+    out, err = capsys.readouterr()
+    for line in err.splitlines():
+        assert "spy/main.py" not in line
+    assert "  Fragment 1" in err.splitlines()
+    assert "    --filter 'this_name_does_not_exist_either'" in err.splitlines()
+    with pytest.raises(NameError):
+        spy.cli._main(sys.argv[0], '--no-exception-handling', 'still_broken_i_hope')
+
+
+def test_run(capsys, monkeypatch):
     input = "this is\na piece of sample input\nused to test a complete run"
     expected = "SAMPLE PIECE THIS\nTEST USED INPUT\nRUN COMPLETE\n"
-    stdin = sys.stdin
-    argv = sys.argv
-    try:
-        sys.stdin = io.StringIO(input)
-        sys.argv = sys.argv[0:1] + [
-                '-l',
-                'spy.many(pipe.split())',
-                '-f', 'len(pipe) > 2',
-                'pipe.upper()',
-                'list(itertools.islice(spy.collect(), 3))',
-                '" ".join(reversed(pipe))']
-        with pytest.raises(SystemExit):
-            from spy import __main__
-        out, err = capsys.readouterr()
-        assert err == ''
-        assert out == expected
-    finally:
-        sys.stdin = stdin
-        sys.argv = argv
+    monkeypatch.setattr(sys, 'stdin', io.StringIO(input))
+    monkeypatch.setattr(sys, 'argv',
+        sys.argv[0:1] + [
+            '-l',
+            'spy.many(pipe.split())',
+            '-f', 'len(pipe) > 2',
+            'pipe.upper()',
+            'list(itertools.islice(spy.collect(), 3))',
+            '" ".join(reversed(pipe))'])
+    with pytest.raises(SystemExit):
+        from spy import __main__
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == expected
 
 
-def test_show_fragments(capsys):
+def test_show_fragments(capsys, monkeypatch):
     expected = '''
   1 | <cli> --filter --callable 'int'
   2 | <cli> --accumulate --accumulate 'asdfgfa'
   3 | <cli> --many 'test'
   4 | <cli> --many --many 'baz'
 '''
-    stdin = sys.stdin
-    try:
-        sys.stdin = io.StringIO('')
-        argv = sys.argv[0:1] + [
-                '--show-fragments',
-                '-l',
-                '-fc', 'int',
-                '--accumulate', '-a', 'asdfgfa',
-                '-m', 'test',
-                '-m', '--many', 'baz']
-        spy.cli._main(*argv)
-        out, err = capsys.readouterr()
-        assert expected in out
-    finally:
-        sys.stdin = stdin
+    monkeypatch.setattr(sys, 'stdin', io.StringIO(""))
+    argv = sys.argv[0:1] + [
+            '--show-fragments',
+            '-l',
+            '-fc', 'int',
+            '--accumulate', '-a', 'asdfgfa',
+            '-m', 'test',
+            '-m', '--many', 'baz']
+    spy.cli._main(*argv)
+    out, err = capsys.readouterr()
+    assert expected in out
