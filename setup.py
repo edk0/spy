0e5feb2c855013213f1bb69c84b598435d509e36
Ed Kellett <e@kellett.im>
2018-08-03 17:15:43 +0100

    cut a new version, i guess

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='spy-cli',
-    version='0.2',
+    version='0.3',
     description='stream processing Python CLI',
     packages=['spy'],
     zip_safe=True,
