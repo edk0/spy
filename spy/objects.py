feffda8ff07ba1923d12836a1826e9c0cf1cd98a
Ed Kellett <e@kellett.im>
2019-11-30 17:26:30 +0000

    Remove _FunctionWrapper.__index__

--- spy/objects.py
+++ spy/objects.py
@@ -151,7 +151,6 @@ class _FunctionWrapper:
     __complex__ = _proxy_unop(complex)
     __int__ = _proxy_unop(int)
     __float__ = _proxy_unop(float)
-    __index__ = _proxy_unop(operator.index)
     __trunc__ = _proxy_unop(math.trunc)
     __floor__ = _proxy_unop(math.floor)
     __ceil__ = _proxy_unop(math.ceil)
