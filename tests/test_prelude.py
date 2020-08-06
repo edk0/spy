ed5494429ac09e18a0c1b4a358893ccc12ebba16
Ed Kellett <e@kellett.im>
2020-08-06 18:08:27 +0100

    Test prelude.sum

--- tests/test_prelude.py
+++ tests/test_prelude.py
@@ -9,3 +9,18 @@ def test_ft():
     def f(x):
         return x + 1
     assert prelude.ft(f, f, f)(1) == (2, 2, 2)
+
+
+def test_sum():
+    l = [1,2,3]
+    assert prelude.sum(l) == 6
+
+    l = [1.,2.,3.]
+    assert prelude.sum(l) == sum(l)
+
+    l = ['abc', 'def', 'ghi']
+    assert prelude.sum(l, '') == 'abcdefghi'
+
+    l = [[1], [2, 3], [4, 5, 6]]
+    assert prelude.sum(l, []) == [1, 2, 3, 4, 5, 6]
+    assert l == [[1], [2, 3], [4, 5, 6]]
