c3fabe21d490db48af3cfa11ec3bc801475bfacc
Ed Kellett <e@kellett.im>
2021-07-18 22:28:34 +0100

    Update docs copyright

--- doc/conf.py
+++ doc/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'spy'
-copyright = '2014–2019, Ed Kellett'
+copyright = '2014–2021, Ed Kellett & contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
